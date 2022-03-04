a24e877e692dc0949e101f157c1f6da717bb0c81
inhwanjeong <ghjklla007@naver.com>
2022-03-04 13:42:23 +0900

    django db insert 및 select

--- Django/HelloDjango/pybo/views.py
+++ Django/HelloDjango/pybo/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.http import HttpResponse
 from .models import Question
+from django.utils import timezone
 
 
 # Create your views here.
 def index(requset):
-    question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
-    return render(request, 'pybo/question_list.html', context)
+    # question_list = Question.objects.order_by('-create_date')
+    # context = {'question_list': question_list}
+    # return render(request, 'pybo/question_list.html', context)
     # return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+
+    q = Question(subject='pybo가 무엇인가요?', content='pybo에 대해서 알고 싶습니다.', create_date=timezone.now())
+    q.save()
+
+    question_item = Question.objects.get(id=2)
+    res_text = str(question_item) + str(question_item.content)
+
+    return HttpResponse(res_text)
