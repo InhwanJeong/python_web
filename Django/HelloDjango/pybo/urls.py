36ee964c229870018590221bc240713004310a0a
inan <ghjklla007@naver.com>
2022-03-05 19:31:07 +0900

    [add] django ORM

--- Django/HelloDjango/pybo/urls.py
+++ Django/HelloDjango/pybo/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('orm', views.orm_cook_book),
 ]
--- Django/HelloDjango/pybo/views.py
+++ Django/HelloDjango/pybo/views.py
@@ -14,7 +14,23 @@ def index(requset):
     q = Question(subject='pybo가 무엇인가요?', content='pybo에 대해서 알고 싶습니다.', create_date=timezone.now())
     q.save()
 
-    question_item = Question.objects.get(id=2)
-    res_text = str(question_item) + str(question_item.content)
+    question_queryset = Question.objects.get(id=2)
+    res_text = str(question_queryset) + str(question_queryset.content)
 
     return HttpResponse(res_text)
+
+def orm_cook_book(request):
+    # 1. 장고 ORM이 실행하는 실제 SQL 질의문을 확인
+
+    # SELECT "pybo_question"."id", "pybo_question"."subject",
+    # "pybo_question"."content", "pybo_question"."create_date" FROM "pybo_question"
+    question_queryset = Question.objects.all()
+
+    query = str(question_queryset.query)
+
+    # SELECT "pybo_question"."id", "pybo_question"."subject", "pybo_question"
+    # ."content", "pybo_question"."create_date" FROM "pybo_question" WHERE "pybo_question"."id" = 5
+    question_queryset = Question.objects.filter(id=5)
+    query = str(question_queryset.query)
+
+    return HttpResponse(query)
